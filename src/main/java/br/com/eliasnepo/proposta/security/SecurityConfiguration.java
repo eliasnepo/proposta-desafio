2b9760261fbb724f1efeb629b638ac84b6d114d8
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-07-21 00:02:41 -0300

    Adiciona login via senha

---
+++ src/main/java/br/com/eliasnepo/proposta/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.eliasnepo.proposta.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_propostas:read")
+                                .antMatchers(HttpMethod.POST, "/propostas").hasAuthority("SCOPE_propostas:write")
+                                .antMatchers(HttpMethod.POST, "/biometrias/**").hasAuthority("SCOPE_biometrias:write")
+                                .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_actuator:read")
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
